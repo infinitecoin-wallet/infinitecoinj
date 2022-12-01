7a238578d70fe866e91e0a65ec0dc46e7c742c4e
infinitecoin-core <59826757+infinitecoin-core@users.noreply.github.com>
2022-12-01 16:38:32 +0800

    The initial block reward for Infinitecoin is 524288
    The initial block reward for Bitcoin and Litecoin is 50

--- core/src/main/java/org/bitcoinj/core/Coin.java
+++ core/src/main/java/org/bitcoinj/core/Coin.java
@@ -71,7 +71,7 @@ public final class Coin implements Monetary, Comparable<Coin>, Serializable {
      */
     public static final Coin SATOSHI = Coin.valueOf(1);
 
-    public static final Coin FIFTY_COINS = COIN.multiply(50);
+    public static final Coin FIFTY_COINS = COIN.multiply(524288);
 
     /**
      * Represents a monetary value of minus one satoshi.
